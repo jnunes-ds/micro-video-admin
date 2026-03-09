5a52c541cba55b1d1d527726b7ee6fac285b42d8
Denison Junior <denisonprobable13@gmail.com>
2026-03-09 19:00:41 -0300

    feat: add `CategoryModelMapper` to map between entity and Sequelize model
    - Introduced a utility for transforming `Category` entities to `CategoryModel` and vice versa.

---
+++ src/category/infra/db/sequelize/category_mapper.ts
@@ -0,0 +1,25 @@
+import {Category} from "@/category/domain/category.entity";
+import {CategoryModel} from "@/category/infra/db/sequelize/category.model";
+import {Uuid} from "@/@shared/domain/value_objects/uuid.vo";
+
+export class CategoryModelMapper {
+	static toModel(entity: Category): CategoryModel {
+		return CategoryModel.build({
+			category_id: entity.category_id.id,
+			name: entity.name,
+			description: entity.description,
+			is_active: entity.is_active,
+			created_at: entity.created_at,
+		});
+	}
+
+	static toEntity(model: CategoryModel): Category {
+		return new Category({
+			category_id: new Uuid(model.category_id),
+			name: model.name,
+			description: model.description,
+			is_active: model.is_active,
+			created_at: model.created_at
+		});
+	}
+}
