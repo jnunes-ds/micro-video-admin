44367d06c577249feb9de91408608c05e30376f5
Denison Junior <denisonprobable13@gmail.com>
2026-03-11 19:43:39 -0300

    test: add `GetCategoryUsecase` for retrieving category details
    - Implemented `GetCategoryUsecase` to fetch category details by ID.
    - Integrated `ICategoryRepository` for category lookup.
    - Added error handling with `NotFoundError` for non-existent categories.
    - Defined input and output types for consistent type safety.

---
+++ src/category/application/__tests__/unit/get_category.usecase.spec.ts
@@ -0,0 +1,42 @@
+import {CategoryInMemoryRepository} from "@/category/infra/db/in_memory/category_in_memory.repository";
+import {GetCategoryUsecase} from "@/category/application/get_category.usecase";
+import {InvalidUuidError, Uuid} from "@/@shared/domain/value_objects/uuid.vo";
+import {NotFoundError} from "@/@shared/domain/errors/not_found.error";
+import {Category} from "@/category/domain/category.entity";
+
+describe('GetCategoryUsecase Unit Tests', () => {
+	let usecase: GetCategoryUsecase;
+	let repository: CategoryInMemoryRepository;
+
+	beforeEach(() => {
+		repository = new CategoryInMemoryRepository();
+		usecase = new GetCategoryUsecase(repository);
+	});
+
+	it('should throws an error when entity is not found', async () => {
+		await expect(
+			() => usecase.execute({id: 'fake id'})
+		).rejects.toThrow(new InvalidUuidError());
+
+		const uuid = new Uuid();
+
+		await expect(
+			() => usecase.execute({id: uuid.id })
+		).rejects.toThrow(new NotFoundError(uuid, Category));
+	});
+
+	it('should get a category by id', async () => {
+		const items = Category.fake()
+			.theCategories(11)
+			.withName(index => `name-${index + 1}`)
+			.build();
+		repository.items = items;
+
+		const spy = jest.spyOn(repository, 'findById');
+		const seventhItemId = items[6].category_id.id;
+		const output = await usecase.execute({id: seventhItemId});
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(output.name).toBe('name-7');
+	});
+});
