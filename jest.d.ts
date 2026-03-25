c563c4d5eac17086d2336a922ca845fcc8e78948
Denison Junior <denisonprobable13@gmail.com>
2026-03-25 15:10:29 -0300

    test: add custom Jest matcher for Notification errors

--- jest.d.ts
+++ jest.d.ts
@@ -1,9 +1,13 @@
 import {FieldsErrors} from "@/@shared/domain/validators/validator_fields.interface";
 
+
 declare global {
 	namespace jest {
 		interface Matchers<R> {
-			containsErrorMessages(expected: FieldsErrors): R;
+			// containsErrorMessages(expected: FieldsErrors): R;
+			notificationContainsErrorMessages(
+				expect: Array<string | { [key: string]: string[] }>
+			): R;
 		}
 	}
 }
