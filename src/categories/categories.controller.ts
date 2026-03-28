861f3c71c584c06895dc53c067c22675a89eb8df
Denison Junior <denisonprobable13@gmail.com>
2026-03-28 15:38:24 -0300

    feat: integrate Sequelize and CategorySequelizeRepository into CategoriesModule
    - Configured `SequelizeModule` with SQLite and registered `CategoryModel`.
    - Provided `CategorySequelizeRepository` to `CategoriesModule` using `useFactory`.
    - Updated `CategoriesController` to inject `CategorySequelizeRepository`.

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import {CategorySequelizeRepository} from "@core/category/infra/db/sequelize/category-sequelize.repository";
 
 @Controller('categories')
 export class CategoriesController {
-  constructor() {}
+  constructor(private categoryRepo: CategorySequelizeRepository) {
+    console.log(this.categoryRepo);
+  }
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
