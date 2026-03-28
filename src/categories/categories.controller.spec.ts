7e9b6f02343d6273248a6d7c83835873b93ae73a
Denison Junior <denisonprobable13@gmail.com>
2026-03-28 17:11:22 -0300

    test: replace inline Sequelize configuration with DatabaseModule and add ConfigModule

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,32 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import {CategorySequelizeRepository} from "@core/category/infra/db/sequelize/category-sequelize.repository";
-import {CategoryModel} from "@core/category/infra/db/sequelize/category.model";
-import {getModelToken, SequelizeModule} from "@nestjs/sequelize";
+import {DatabaseModule} from "@/database/database.module";
+import {CategoriesModule} from "@/categories/categories.module";
+import {ConfigModule} from "@/config/config.module";
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        SequelizeModule.forRoot({
-          dialect: 'sqlite',
-          host: ':memory:',
-          logging: false,
-          models: [CategoryModel]
-        }),
-        SequelizeModule.forFeature([CategoryModel])
-      ],
-      controllers: [CategoriesController],
-      providers: [
-        {
-          provide: CategorySequelizeRepository,
-          useFactory: (categoryModel: typeof CategoryModel) =>
-            new CategorySequelizeRepository(categoryModel),
-          inject: [getModelToken(CategoryModel)]
-        }
-      ]
+      imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
