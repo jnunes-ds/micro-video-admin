9a53269b5c697cbe61bf4d757ddace9af8cd37fa
Denison Junior <denisonprobable13@gmail.com>
2026-04-08 15:01:31 -0300

    feat: inject category use cases into controller
    - Replaced direct repository usage in `CategoriesController` with dependency injection of CRUD use cases.
    - Added `CreateCategoryUsecase`, `UpdateCategoryUseCase`, `DeleteCategoryUsecase`, `GetCategoryUsecase`, and `ListCategoriesUseCase` as injectable properties to improve separation of concerns and modularity.

--- src/nest-modules/categories/categories.controller.ts
+++ src/nest-modules/categories/categories.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, Inject} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import {CategorySequelizeRepository} from "@core/category/infra/db/sequelize/category-sequelize.repository";
+import {CreatecategoryUsecase} from "@core/category/application/usecases/create_category/create_category.usecase";
+import {UpdateCategoryUseCase} from "@core/category/application/usecases/update_category/update_category.usecase";
+import {DeleteCategoryUsecase} from "@core/category/application/usecases/delete_category/delete_category.usecase";
+import {GetCategoryUsecase} from "@core/category/application/usecases/get_category/get_category.usecase";
+import {ListCategoriesUseCase} from "@core/category/application/usecases/list_categories/list_categories.usecase";
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private categoryRepo: CategorySequelizeRepository) {
-    console.log(this.categoryRepo);
-  }
+
+  @Inject(CreatecategoryUsecase)
+  private createUsecase: CreatecategoryUsecase;
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUsecase: UpdateCategoryUseCase;
+
+  @Inject(DeleteCategoryUsecase)
+  private deleteUsecase: DeleteCategoryUsecase;
+
+  @Inject(GetCategoryUsecase)
+  private getUsecase: GetCategoryUsecase;
+
+  @Inject(ListCategoriesUseCase)
+  private listUsecase: ListCategoriesUseCase;
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
