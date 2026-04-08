98a1817dd6bd59cc54ae3b46004f3f33571487b3
Denison Junior <denisonprobable13@gmail.com>
2026-04-08 09:35:49 -0300

    feat: add use case providers for category module
    - Added `USE_CASES` to `categories.providers.ts` for registering category use case providers.
    - Defined and configured dependency injection for CRUD use cases: create, update, list, get, and delete.
    - Refactored generic providers type for better reusability.

--- src/nest-modules/categories/categories.providers.ts
+++ src/nest-modules/categories/categories.providers.ts
@@ -3,6 +3,16 @@ import {CategoryInMemoryRepository} from "@core/category/infra/db/in_memory/cate
 import {Provider} from "@nestjs/common/interfaces/modules/provider.interface";
 import {CategoryModel} from "@core/category/infra/db/sequelize/category.model";
 import {getModelToken} from "@nestjs/sequelize";
+import {CreatecategoryUsecase} from "@core/category/application/usecases/create_category/create_category.usecase";
+import {ICategoryRepository} from "@core/category/domain/category.repository";
+import {UpdateCategoryUseCase} from "@core/category/application/usecases/update_category/update_category.usecase";
+import {ListCategoriesUseCase} from "@core/category/application/usecases/list_categories/list_categories.usecase";
+import {GetCategoryUsecase} from "@core/category/application/usecases/get_category/get_category.usecase";
+import {DeleteCategoryUsecase} from "@core/category/application/usecases/delete_category/delete_category.usecase";
+
+type Providers<T extends string> = {
+	[key in T]: Provider;
+};
 
 enum RepositoriesKeysEnum {
 	CATEGORY_REPOSITORY = 'CATEGORY_REPOSITORY',
@@ -10,9 +20,7 @@ enum RepositoriesKeysEnum {
 	CATEGORY_SEQUELIZE_REPOSITORY = 'CATEGORY_SEQUELIZE_REPOSITORY'
 }
 
-type Repositories = {
-	[key in RepositoriesKeysEnum]: Provider;
-};
+type Repositories = Providers<RepositoriesKeysEnum>
 
 export const REPOSITORIES: Repositories = {
 	CATEGORY_REPOSITORY: {
@@ -28,4 +36,47 @@ export const REPOSITORIES: Repositories = {
 		useFactory: (categoryModel: typeof CategoryModel) => new CategorySequelizeRepository(categoryModel),
 		inject: [getModelToken(CategoryModel)]
 	}
+}
+
+enum UseCasesEnum  {
+	CREATE_CATEGORY_USE_CASE = 'CREATE_CATEGORY_USE_CASE',
+	UPDATE_CATEGORY_USE_CASE = 'UPDATE_CATEGORY_USE_CASE',
+	LIST_CATEGORIES_USE_CASE = 'LIST_CATEGORIES_USE_CASE',
+	GET_CATEGORY_USE_CASE = 'GET_CATEGORY_USE_CASE',
+	DELETE_CATEGORY_USE_CASE = 'DELETE_CATEGORY_USE_CASE'
+}
+
+type Usecases = Providers<UseCasesEnum>;
+
+export const USE_CASES: Usecases = {
+	CREATE_CATEGORY_USE_CASE: {
+		provide: CreatecategoryUsecase,
+		useFactory: (categoryRepo: ICategoryRepository) => new CreatecategoryUsecase(categoryRepo),
+		inject: [REPOSITORIES.CATEGORY_REPOSITORY['provide']]
+	},
+	UPDATE_CATEGORY_USE_CASE: {
+		provide: UpdateCategoryUseCase,
+		useFactory: (categoryRepo: ICategoryRepository) => new UpdateCategoryUseCase(categoryRepo),
+		inject: [REPOSITORIES.CATEGORY_REPOSITORY['provide']]
+	},
+	LIST_CATEGORIES_USE_CASE: {
+		provide: ListCategoriesUseCase,
+		useFactory: (categoryRepo: ICategoryRepository) => new ListCategoriesUseCase(categoryRepo),
+		inject: [REPOSITORIES.CATEGORY_REPOSITORY['provide']]
+	},
+	GET_CATEGORY_USE_CASE: {
+		provide: GetCategoryUsecase,
+		useFactory: (categoryRepo: ICategoryRepository) => new GetCategoryUsecase(categoryRepo),
+		inject: [REPOSITORIES.CATEGORY_REPOSITORY['provide']]
+	},
+	DELETE_CATEGORY_USE_CASE: {
+		provide: DeleteCategoryUsecase,
+		useFactory: (categoryRepo: ICategoryRepository) => new DeleteCategoryUsecase(categoryRepo),
+		inject: [REPOSITORIES.CATEGORY_REPOSITORY['provide']]
+	}
+}
+
+export const CATEGORY_PROVIDERS = {
+	REPOSITORIES,
+	USE_CASES,
 }
