1e0fb268689f087234ad5fa0ca1ab33339ee4586
Denison Junior <denisonprobable13@gmail.com>
2026-04-08 15:48:38 -0300

    refactor: use `HttpStatus.UNPROCESSABLE_ENTITY` for global validation pipe error code
    - Replaced numeric status code `422` with `HttpStatus.UNPROCESSABLE_ENTITY` for improved readability and maintainability.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import {NestFactory, Reflector} from '@nestjs/core';
-import { AppModule } from './app.module';
-import {ClassSerializerInterceptor, ValidationPipe} from "@nestjs/common";
+import {AppModule} from './app.module';
+import {ClassSerializerInterceptor, HttpStatus, ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({errorHttpStatusCode: 422}));
+  app.useGlobalPipes(new ValidationPipe({errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY}));
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
