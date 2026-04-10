535c040c15e9ddccdbb2adee455ac099e6f2f8d7
Denison Junior <denisonprobable13@gmail.com>
2026-04-10 14:31:36 -0300

    fix: adjust `CategoryCollectionPresenter` to dynamically calculate pagination metadata
    - Updated constructor to accept modified params and compute `last_page` and `total` dynamically.
    - Introduced `CategoryCollectionPresenterParams` type for improved type safety.

--- src/nest-modules/categories/categories.presenter.ts
+++ src/nest-modules/categories/categories.presenter.ts
@@ -5,6 +5,8 @@ import {
 } from "@core/category/application/usecases/list_categories/list_categories.usecase";
 import {CollectionPresenter} from "@/shared/collection.presenter";
 
+type CategoryCollectionPresenterParams = Omit<ListCategoriesOutput, 'last_page' | 'total'>;
+
 export class CategoryPresenter {
 	id: string;
 	name: string;
@@ -25,10 +27,13 @@ export class CategoryPresenter {
 export class CategoryCollectionPresenter extends CollectionPresenter {
 	data: CategoryPresenter[];
 
-	constructor(output: ListCategoriesOutput) {
+	constructor(output: CategoryCollectionPresenterParams) {
 		const {items, ...paginationProps} = output;
-		super(paginationProps);
-
+		super({
+			...paginationProps,
+			last_page: Math.ceil(items.length / paginationProps.per_page),
+			total: items.length,
+		});
 		this.data = items.map(item => new CategoryPresenter(item));
 	}
 }
