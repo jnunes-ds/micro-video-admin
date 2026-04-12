f77f064a8b1f144adfc4d63914b318c56a17020d
Denison Junior <denisonprobable13@gmail.com>
2026-04-12 15:26:03 -0300

    test: consolidate `CategoriesController` integration tests and introduce fixtures
    - Refactored `categories.controller.test.ts` to simplify integration tests, consolidating them for `create`, `update`, `findOne`, `search`, and `remove` methods.
    - Introduced `category_fixture.ts` to streamline test data generation and validation scenarios.
    - Removed redundant or outdated test implementations.

--- src/nest-modules/categories/__tests__/categories.controller.test.ts
+++ src/nest-modules/categories/__tests__/categories.controller.test.ts
@@ -1,195 +1,55 @@
-import { CategoriesController } from '../categories.controller';
-import {CreateCategoryOutput} from "@core/category/application/usecases/create_category/create_category.usecase";
-import {CreateCategoryDto} from "@/nest-modules/categories/dto/create-category.dto";
-import {CategoryPresenter, CategoryCollectionPresenter} from "@/nest-modules/categories/categories.presenter";
-
-// beforeEach(async () => {
-//   const module: TestingModule = await Test.createTestingModule({
-//     imports: [ConfigModule.forRoot(), CategoriesModule],
-//   }).overrideProvider(getModelToken(CategoryModel))
-//     .useValue({})
-//     .overrideProvider('CategoryRepository')
-//     .useValue(REPOSITORIES.CATEGORY_IN_MEMORY_REPOSITORY.provide)
-//     .compile();
-//
-//   controller = module.get<CategoriesController>(CategoriesController);
-// });
+import {CategoriesController} from "@/nest-modules/categories/categories.controller";
+import {ICategoryRepository} from "@core/category/domain/category.repository";
+import {Test, TestingModule} from "@nestjs/testing";
+import {ConfigModule} from "@/nest-modules/config/config.module";
+import {DatabaseModule} from "@/nest-modules/database/database.module";
+import {CategoriesModule} from "@/nest-modules/categories/categories.module";
+import {CATEGORY_PROVIDERS} from "@/nest-modules/categories/categories.providers";
+import {CreatecategoryUsecase} from "@core/category/application/usecases/create_category/create_category.usecase";
+import {UpdateCategoryUsecase} from "@core/category/application/usecases/update_category/update_category.usecase";
+import {DeleteCategoryUsecase} from "@core/category/application/usecases/delete_category/delete_category.usecase";
+import {GetCategoryUsecase} from "@core/category/application/usecases/get_category/get_category.usecase";
+import {ListCategoriesUsecase} from "@core/category/application/usecases/list_categories/list_categories.usecase";
+import {CreateCategoryFixture} from "@/nest-modules/categories/testing/category_fixture";
+import {CategoryPresenter} from "@/nest-modules/categories/categories.presenter";
+import {CategoryOutputMapper} from "@core/category/application/usecases/common/category_output";
+import {Uuid} from "@core/@shared/domain/value_objects/uuid.vo";
 
 describe('CategoriesController Integration Tests', () => {
   let controller: CategoriesController;
+	let repository: ICategoryRepository;
 
-  beforeEach(async () => {
-    controller = new CategoriesController();
-  });
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule]
+		}).compile();
+		controller = module.get<CategoriesController>(CategoriesController);
+		repository = module.get<ICategoryRepository>(
+			CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide
+		);
+	});
 
-  test('category creation', async () => {
-    // ARRANGE
-    const output: CreateCategoryOutput = {
-      id: '324dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds52',
-      name: 'Movie',
-      description: null,
-      is_active: true,
-      created_at: new Date(),
-    };
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+		expect(controller['createUsecase']).toBeInstanceOf(CreatecategoryUsecase);
+		expect(controller['updateUsecase']).toBeInstanceOf(UpdateCategoryUsecase);
+		expect(controller['getUsecase']).toBeInstanceOf(GetCategoryUsecase);
+		expect(controller['listUsecase']).toBeInstanceOf(ListCategoriesUsecase);
+		expect(controller['deleteUsecase']).toBeInstanceOf(DeleteCategoryUsecase);
+	});
 
-    const mockCreateUser = {
-      execute: jest.fn().mockResolvedValue(Promise.resolve(output))
-    };
-
-    // @ts-expect-error
-    controller['createUsecase'] = mockCreateUser;
-
-    const input: CreateCategoryDto = {
-      name: 'Movie',
-      description: 'some description',
-      is_active: true
-    };
-
-    // ACT
-    const presenter = await controller.create(input);
-
-    // ASSERT
-    expect(mockCreateUser.execute).toHaveBeenCalledWith(input);
-    expect(presenter).toBeInstanceOf(CategoryPresenter);
-    expect(presenter).toStrictEqual(new CategoryPresenter(output));
-  });
-
-  test('category update', async () => {
-    // ARRANGE
-    const output = {
-      id: '324dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds52',
-      name: 'Movie Updated',
-      description: 'updated description',
-      is_active: false,
-      created_at: new Date(),
-    };
-
-    const mockUpdateUsecase = {
-      execute: jest.fn().mockResolvedValue(output),
-    };
-
-// @ts-expect-error
-    controller['updateUsecase'] = mockUpdateUsecase;
-
-    const input = {
-      id: '324dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds52',
-      name: 'Movie Updated',
-      description: 'updated description',
-      is_active: false,
-    };
-
-// ACT
-    const presenter = await controller.update(input.id, input);
-
-// ASSERT
-    expect(mockUpdateUsecase.execute).toHaveBeenCalledWith(input);
-    expect(presenter).toBeInstanceOf(CategoryPresenter);
-    expect(presenter).toStrictEqual(new CategoryPresenter(output));
-  });
-
-  test('get category', async () => {
-    // ARRANGE
-    const output = {
-      id: '324dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds52',
-      name: 'Movie',
-      description: 'description',
-      is_active: true,
-      created_at: new Date(),
-    };
-
-    const mockGetUsecase = {
-      execute: jest.fn().mockResolvedValue(output),
-    };
-
-    // @ts-expect-error
-    controller['getUsecase'] = mockGetUsecase;
-
-    const input = {
-      id: '324dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds52',
-    };
-
-    // ACT
-    const presenter = await controller.findOne(input.id);
-
-    // ASSERT
-    expect(mockGetUsecase.execute).toHaveBeenCalledWith(input);
-    expect(presenter).toBeInstanceOf(CategoryPresenter);
-    expect(presenter).toStrictEqual(new CategoryPresenter(output));
-  });
-
-  test('category listage', async () => {
-    // ARRANGE
-    const items = [
-      {
-        id: '324dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds52',
-        name: 'Movie 1',
-        description: 'description 1',
-        is_active: true,
-        created_at: new Date(),
-      },
-      {
-        id: '424dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds53',
-        name: 'Movie 2',
-        description: 'description 2',
-        is_active: false,
-        created_at: new Date(),
-      },
-    ]
-    const output = {
-      items,
-      current_page: 1,
-      per_page: 10,
-      total: 2,
-      last_page: 1,
-    };
-
-    const mockListUsecase = {
-      execute: jest.fn().mockResolvedValue(Promise.resolve(output)),
-    };
-
-    // @ts-expect-error
-    controller['listUsecase'] = mockListUsecase;
-
-    const input = {
-      items,
-      page: 1,
-      per_page: 10,
-      sort: undefined,
-      sort_dir: undefined,
-      filter: undefined,
-    };
-
-    // ACT
-    const presenter = await controller.search(input);
-
-    // ASSERT
-    expect(mockListUsecase.execute).toHaveBeenCalledWith(input);
-    expect(presenter).toBeInstanceOf(CategoryCollectionPresenter);
-    expect(presenter).toStrictEqual(new CategoryCollectionPresenter(output));
-  });
-
-  test('category deletion', async () => {
-    // ARRANGE
-    const mockDeleteUsecase = {
-      execute: jest.fn().mockResolvedValue(undefined),
-    };
-
-    // @ts-expect-error
-    controller['deleteUsecase'] = mockDeleteUsecase;
-
-    const input = {
-      id: '324dsfw32431-432dsa-4235bfd-43252gf-6t43f64fds52',
-    };
-
-    // ACT
-    const presenter = await controller.remove(input.id);
-
-    // ASSERT
-    expect(mockDeleteUsecase.execute).toHaveBeenCalledWith(input);
-    expect(presenter).toBeUndefined();
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	describe('create categories', () => {
+		const arrange = CreateCategoryFixture.arrangeForCreate();
+		test.each(arrange)('when body is $send_data', async ({ send_data, expected }) => {
+			const presenter = await controller.create(send_data);
+			const entity = await repository.findById(new Uuid(presenter.id));
+			expect(entity.toJSON()).toStrictEqual({
+				category_id: presenter.id,
+				created_at: presenter.created_at,
+				...expected
+			});
+			const output = CategoryOutputMapper.toOutput(entity);
+			expect(presenter).toEqual(new CategoryPresenter(output));
+		})
+	});
 });
