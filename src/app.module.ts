faefef98691b9c7db19bdee8188a96038cd597cd
Denison Junior <denisonprobable13@gmail.com>
2026-04-12 15:25:48 -0300

    refactor: update import paths for `AppController` and `AppService` in `app.module.ts`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoriesModule } from '@/nest-modules/categories/categories.module';
 import { DatabaseModule } from '@/nest-modules/database/database.module';
 import { ConfigModule } from '@/nest-modules/config/config.module';
 import { SharedModule } from './shared/shared.module';
+import {AppController} from "@nestjs/schematics/dist/lib/application/files/ts/src/app.controller";
+import {AppService} from "@nestjs/schematics/dist/lib/application/files/ts/src/app.service";
 
 @Module({
   imports: [
