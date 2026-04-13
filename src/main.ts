74cec73f964d70ed9b3d31d7eaad4825b961555b
Denison Junior <denisonprobable13@gmail.com>
2026-04-13 16:28:36 -0300

    feat: add `WrapperDataInterceptor` and apply it globally
    - Implemented `WrapperDataInterceptor` to wrap response data under `data` key unless `meta` is present.
    - Registered `WrapperDataInterceptor` as a global interceptor in `main.ts`.
    - Added unit tests to validate `WrapperDataInterceptor` behavior.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {NestFactory, Reflector} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ClassSerializerInterceptor, HttpStatus, ValidationPipe} from "@nestjs/common";
+import {WrapperDataInterceptor} from "@/nest-modules/shared/interceptors/wrapper-data/wrapper-data.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY}));
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new WrapperDataInterceptor())
 
   await app.listen(process.env.PORT ?? 3000);
 
