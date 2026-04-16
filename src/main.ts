1778a55ff3125d551cdf289d235fa278b0512286
Denison Junior <denisonprobable13@gmail.com>
2026-04-16 17:27:13 -0300

    feat: add `EntityValidationErrorFilter` and register it globally in `main.ts`
    - Implemented `EntityValidationErrorFilter` to handle `EntityValidationError` exceptions.
    - Registered `EntityValidationErrorFilter` as a global filter alongside `NotFoundErrorFilter` in `main.ts`.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {AppModule} from './app.module';
 import {ClassSerializerInterceptor, HttpStatus, ValidationPipe} from "@nestjs/common";
 import {NotFoundErrorFilter} from "@/nest-modules/shared/not_found/not_found_error.filter";
 import {WrapperDataInterceptor} from "@/nest-modules/shared/interceptors/wrapper_data/wrapper_data.interceptor.service";
+import {EntityValidationErrorFilter} from "@/nest-modules/shared/not_found/entity_validation_error.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new WrapperDataInterceptor());
-  app.useGlobalFilters(new NotFoundErrorFilter());
+  app.useGlobalFilters(new NotFoundErrorFilter(), new EntityValidationErrorFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 
