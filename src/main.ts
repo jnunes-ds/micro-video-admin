e6914e431fd579b713eb13f69a6815b918337ea4
Denison Junior <denisonprobable13@gmail.com>
2026-04-16 17:26:50 -0300

    refactor: update `NotFoundFilter` reference to `NotFoundErrorFilter` in `main.ts`

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import {NestFactory, Reflector} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ClassSerializerInterceptor, HttpStatus, ValidationPipe} from "@nestjs/common";
-import {NotFoundFilter} from "@/nest-modules/shared/not_found/not_found_error.filter";
+import {NotFoundErrorFilter} from "@/nest-modules/shared/not_found/not_found_error.filter";
 import {WrapperDataInterceptor} from "@/nest-modules/shared/interceptors/wrapper_data/wrapper_data.interceptor.service";
 
 async function bootstrap() {
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new WrapperDataInterceptor());
-  app.useGlobalFilters(new NotFoundFilter());
+  app.useGlobalFilters(new NotFoundErrorFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 
