0dbdaae2415aec00367dbf2876d7bc5cd3121a2f
Denison Junior <denisonprobable13@gmail.com>
2026-04-16 17:15:13 -0300

    feat: implement `NotFoundFilter` and register it globally
    - Added `NotFoundFilter` to handle `NotFoundError` exceptions and return a standardized 404 response.
    - Registered `NotFoundFilter` as a global filter in `main.ts`.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {NestFactory, Reflector} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ClassSerializerInterceptor, HttpStatus, ValidationPipe} from "@nestjs/common";
 import {WrapperDataInterceptor} from "@/nest-modules/shared/interceptors/wrapper-data/wrapper-data.interceptor";
+import {NotFoundFilter} from "@/nest-modules/shared/not_found/not_found.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY}));
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalInterceptors(new WrapperDataInterceptor())
+  app.useGlobalInterceptors(new WrapperDataInterceptor());
+  app.useGlobalFilters(new NotFoundFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 
