a5fe6a346b1220eb44151725cd92971840cb900d
Denison Junior <denisonprobable13@gmail.com>
2026-04-16 21:49:59 -0300

    refactor: extract global app configuration to `applyGlobalConfig` utility
    - Moved global pipes, filters, and interceptors setup to a dedicated `applyGlobalConfig` function in `global_config.ts`.
    - Simplified `main.ts` by delegating global configuration to the extracted utility.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
-import {NestFactory, Reflector} from '@nestjs/core';
+import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import {ClassSerializerInterceptor, HttpStatus, ValidationPipe} from "@nestjs/common";
-import {NotFoundErrorFilter} from "@/nest-modules/shared/filters/not_found_error.filter";
-import {WrapperDataInterceptor} from "@/nest-modules/shared/interceptors/wrapper_data/wrapper_data.interceptor.service";
-import {EntityValidationErrorFilter} from "@/nest-modules/shared/filters/entity_validation_error.filter";
+import {applyGlobalConfig} from "@/nest-modules/global_config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY}));
-
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalInterceptors(new WrapperDataInterceptor());
-  app.useGlobalFilters(new NotFoundErrorFilter(), new EntityValidationErrorFilter());
+  applyGlobalConfig(app);
 
   await app.listen(process.env.PORT ?? 3000);
 
