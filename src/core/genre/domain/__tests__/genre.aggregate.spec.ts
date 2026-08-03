30c52bc706ec133ad9cc32cf067052a09d4cb6a2
Denison Junior <denisonprobable13@gmail.com>
2026-08-03 11:17:57 -0300

    test: create genre aggregate

---
+++ src/core/genre/domain/__tests__/genre.aggregate.spec.ts
@@ -0,0 +1,260 @@
+import {Genre, GenreId} from "../genre.aggregate";
+import {CategoryId} from "../../../category/domain/category.aggregate";
+
+describe('Genre Unit Tests', () => {
+
+	it('should change name', () => {
+		const genre = Genre.fake().aGenre().build();
+		genre.changeName('Drama');
+		expect(genre.name).toBe('Drama');
+	});
+
+	it('should add a category id', () => {
+		const genre = Genre.fake().aGenre().build();
+		const categoryId = new CategoryId();
+
+		genre.addCategoryId(categoryId);
+
+		expect(genre.categories_id.size).toBe(2);
+		expect(genre.categories_id.get(categoryId.id)).toBe(categoryId);
+	});
+
+	it('should not duplicate a category id already added', () => {
+		const categoryId = new CategoryId();
+		const genre = Genre.fake().aGenre().withCategoriesId(categoryId).build();
+
+		genre.addCategoryId(categoryId);
+
+		expect(genre.categories_id.size).toBe(1);
+	});
+
+	it('should remove a category id', () => {
+		const categoryId = new CategoryId();
+		const otherCategoryId = new CategoryId();
+		const genre = Genre.fake()
+			.aGenre()
+			.withCategoriesId(categoryId, otherCategoryId)
+			.build();
+
+		genre.removeCategoryId(categoryId);
+
+		expect(genre.categories_id.size).toBe(1);
+		expect(genre.categories_id.get(categoryId.id)).toBeUndefined();
+		expect(genre.categories_id.get(otherCategoryId.id)).toBe(otherCategoryId);
+	});
+
+	it('should sync the categories id', () => {
+		const genre = Genre.fake().aGenre().build();
+		const categoryId = new CategoryId();
+		const otherCategoryId = new CategoryId();
+
+		genre.syncCategoriesId([categoryId, otherCategoryId]);
+
+		expect(genre.categories_id).toEqual(
+			new Map([
+				[categoryId.id, categoryId],
+				[otherCategoryId.id, otherCategoryId],
+			]),
+		);
+	});
+
+	it('should throw an error when syncCategoriesId receives no categories id', () => {
+		const genre = Genre.fake().aGenre().build();
+
+		expect(() => genre.syncCategoriesId(null as never)).toThrow(
+			new Error('Categories id is empty'),
+		);
+		expect(() => genre.syncCategoriesId(undefined as never)).toThrow(
+			new Error('Categories id is empty'),
+		);
+	});
+
+	it('should activate the genre', () => {
+		const genre = Genre.fake().aGenre().deactivate().build();
+
+		expect(genre.is_active).toBeFalsy();
+		genre.activate();
+		expect(genre.is_active).toBeTruthy();
+	});
+
+	it('should deactivate the genre', () => {
+		const genre = Genre.fake().aGenre().build();
+
+		expect(genre.is_active).toBeTruthy();
+		genre.deactivate();
+		expect(genre.is_active).toBeFalsy();
+	});
+
+	it('should returns the entity_id', () => {
+		const genre = Genre.fake().aGenre().build();
+		expect(genre.entity_id).toBe(genre.gende_id);
+	});
+
+	it('should returns all genre info with a JSON format', () => {
+		const created_at = new Date();
+		const categoryId = new CategoryId();
+		const genre = Genre.fake()
+			.aGenre()
+			.withName('Drama')
+			.withCategoriesId(categoryId)
+			.deactivate()
+			.withCreatedAt(created_at)
+			.build();
+
+		expect(genre.toJSON()).toStrictEqual({
+			genre_id: genre.gende_id.id,
+			name: 'Drama',
+			categories_id: [categoryId.id],
+			is_active: false,
+			created_at,
+		});
+	});
+
+	describe('gende_id field', () => {
+		const arrange: {gende_id: GenreId}[] = [
+			{gende_id: null as never},
+			{gende_id: undefined as never},
+			{gende_id: new GenreId()},
+		];
+		test.each(arrange)('id = %j', ({gende_id}) => {
+			const genre = Genre.fake().aGenre().withGenreId(gende_id).build();
+			expect(genre.gende_id).toBeInstanceOf(GenreId);
+		});
+	});
+
+	describe('Constructor', () => {
+		it('should create a genre with default values', () => {
+			const categoryId = new CategoryId();
+			const genre = new Genre({
+				name: 'Drama',
+				categories_id: new Map([[categoryId.id, categoryId]]),
+			});
+
+			expect(genre.gende_id).toBeInstanceOf(GenreId);
+			expect(genre.name).toBe('Drama');
+			expect(genre.categories_id).toEqual(
+				new Map([[categoryId.id, categoryId]]),
+			);
+			expect(genre.is_active).toBeTruthy();
+			expect(genre.created_at).toBeInstanceOf(Date);
+		});
+
+		it('should create a genre with active property false', () => {
+			const categoryId = new CategoryId();
+			const genre = new Genre({
+				name: 'Drama',
+				categories_id: new Map([[categoryId.id, categoryId]]),
+				is_active: false,
+			});
+
+			expect(genre.gende_id).toBeInstanceOf(GenreId);
+			expect(genre.name).toBe('Drama');
+			expect(genre.is_active).toBeFalsy();
+			expect(genre.created_at).toBeInstanceOf(Date);
+		});
+
+		it('should create a genre with all specific values', () => {
+			const created_at = new Date();
+			const gende_id = new GenreId();
+			const categoryId = new CategoryId();
+			const genre = new Genre({
+				gende_id,
+				name: 'Drama',
+				categories_id: new Map([[categoryId.id, categoryId]]),
+				is_active: false,
+				created_at,
+			});
+
+			expect(genre.gende_id).toBe(gende_id);
+			expect(genre.name).toBe('Drama');
+			expect(genre.categories_id).toEqual(
+				new Map([[categoryId.id, categoryId]]),
+			);
+			expect(genre.is_active).toBeFalsy();
+			expect(genre.created_at).toBe(created_at);
+		});
+	});
+
+	describe('Create Command', () => {
+		it('should create a genre', () => {
+			const categoryId = new CategoryId();
+			const genre = Genre.create({
+				name: 'Drama',
+				categories_id: [categoryId],
+			});
+
+			expect(genre.gende_id).toBeInstanceOf(GenreId);
+			expect(genre.name).toBe('Drama');
+			expect(genre.categories_id).toEqual(
+				new Map([[categoryId.id, categoryId]]),
+			);
+			expect(genre.is_active).toBeTruthy();
+			expect(genre.created_at).toBeInstanceOf(Date);
+			expect(genre.notification.hasErrors()).toBe(false);
+		});
+
+		it('should create a genre with many categories id', () => {
+			const categoryId = new CategoryId();
+			const otherCategoryId = new CategoryId();
+			const genre = Genre.create({
+				name: 'Drama',
+				categories_id: [categoryId, otherCategoryId],
+			});
+
+			expect(genre.categories_id.size).toBe(2);
+			expect(genre.categories_id).toEqual(
+				new Map([
+					[categoryId.id, categoryId],
+					[otherCategoryId.id, otherCategoryId],
+				]),
+			);
+		});
+
+		it('should create a genre with active property false', () => {
+			const genre = Genre.create({
+				name: 'Drama',
+				categories_id: [new CategoryId()],
+				is_active: false,
+			});
+
+			expect(genre.gende_id).toBeInstanceOf(GenreId);
+			expect(genre.name).toBe('Drama');
+			expect(genre.is_active).toBeFalsy();
+			expect(genre.created_at).toBeInstanceOf(Date);
+		});
+	});
+});
+
+describe('Genre Validator', () => {
+	it('should have errors if name is invalid', () => {
+		const genre = Genre.create({
+			name: 't'.repeat(256),
+			categories_id: [new CategoryId()],
+		});
+
+		expect(genre.notification.hasErrors()).toBe(true);
+	});
+
+	it('should not have errors if name is valid', () => {
+		const genre = Genre.create({
+			name: 't'.repeat(255),
+			categories_id: [new CategoryId()],
+		});
+
+		expect(genre.notification.hasErrors()).toBe(false);
+	});
+});
+
+// METHODS
+describe('Methods', () => {
+	it('should print an errors if validate is called after changeName receives an invalid argument', () => {
+		const genre = Genre.create({
+			name: 'Drama',
+			categories_id: [new CategoryId()],
+		});
+		genre.changeName('t'.repeat(256));
+		genre.validate();
+
+		expect(genre.notification.hasErrors()).toBe(true);
+	});
+});
---
+++ src/core/genre/domain/__tests__/genre_fake_builder.spec.ts
@@ -0,0 +1,303 @@
+import Chance from "chance";
+import {Genre, GenreId} from "@core/genre/domain/genre.aggregate";
+import {GenreFakeBuilder} from "@core/genre/domain/genre_fake.builder";
+import {CategoryId} from "@core/category/domain/category.aggregate";
+
+describe('GenreFakeBuilder Unit Test', () => {
+	describe('genre_id prop', () => {
+		const faker = Genre.fake().aGenre();
+
+		it('should throw error when any with methods has been called', () => {
+			expect(() => faker.genre_id).toThrowError(
+				new Error(
+					"Property genre_id not have a factory, use 'with' methods"
+				)
+			);
+		});
+
+		it('should be undefined', () => {
+			expect(faker['_genre_id']).toBeUndefined();
+		});
+
+		test('withGenreId', () => {
+			const genre_id = new GenreId();
+			const $this = faker.withGenreId(genre_id);
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_genre_id']).toBe(genre_id);
+
+			faker.withGenreId(() => genre_id);
+			// @ts-expect-error _genre_id is callable
+			expect(faker['_genre_id']()).toBe(genre_id);
+			expect(faker.genre_id).toBe(genre_id);
+		});
+
+		it('should pass index to genre_id factory', () => {
+			let mockFactory = jest.fn(() => new GenreId());
+			faker.withGenreId(mockFactory);
+			faker.build();
+			expect(mockFactory).toHaveBeenCalledTimes(1);
+
+			const genreId = new GenreId();
+			mockFactory = jest.fn(() => genreId);
+			const fakerMany = Genre.fake().theGenres(2);
+			fakerMany.withGenreId(mockFactory);
+			fakerMany.build();
+
+			expect(mockFactory).toHaveBeenCalledTimes(2);
+			expect(fakerMany.build()[0].gende_id).toBe(genreId);
+			expect(fakerMany.build()[1].gende_id).toBe(genreId);
+		});
+	});
+
+	describe('name prop', () => {
+		const faker = Genre.fake().aGenre();
+
+		it('should be a function', () => {
+			expect(typeof faker['_name']).toBe('function');
+		});
+
+		it('should call the word method', () => {
+			const chance = Chance();
+			const spyWordMethod = jest.spyOn(chance, 'word');
+			faker['chance'] = chance;
+			faker.build();
+			expect(spyWordMethod).toHaveBeenCalled();
+		});
+
+		test('withName', () => {
+			const $this = faker.withName('test');
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_name']).toBe('test');
+
+			faker.withName(() => 'test name');
+
+			// @ts-expect-error _name is callable
+			expect(faker['_name']()).toBe('test name');
+			expect(faker.name).toBe('test name');
+		});
+
+		it('should pass index to name factory', () => {
+			faker.withName((index) => `test name ${index}`);
+			const genre = faker.build();
+			expect(genre.name).toBe(`test name 0`);
+
+			const fakeMany = Genre.fake().theGenres(2);
+			fakeMany.withName((index) => `test name ${index}`);
+			const genres = fakeMany.build();
+
+			expect(genres[0].name).toBe(`test name 0`);
+			expect(genres[1].name).toBe(`test name 1`);
+		});
+
+		test('invalid name to long case', () => {
+			const $this = faker.withInvalidNameTooLong();
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_name'].length).toBe(256);
+
+			const tooLong = 'a'.repeat(256);
+			faker.withInvalidNameTooLong(tooLong);
+			expect(faker['_name']).toBe(tooLong);
+			expect(faker['_name'].length).toBe(256);
+		});
+	});
+
+	describe('categories_id prop', () => {
+		it('should throw error when any with methods has been called', () => {
+			const faker = Genre.fake().aGenre();
+			expect(() => faker.categories_id).toThrowError(
+				new Error(
+					"Property categories_id not have a factory, use 'with' methods"
+				)
+			);
+		});
+
+		it('should be an empty array', () => {
+			const faker = Genre.fake().aGenre();
+			expect(faker['_categories_id']).toStrictEqual([]);
+		});
+
+		it('should create a random category id when no with method has been called', () => {
+			const genre = Genre.fake().aGenre().build();
+			expect(genre.categories_id.size).toBe(1);
+			expect([...genre.categories_id.values()][0]).toBeInstanceOf(CategoryId);
+		});
+
+		test('addCategoryId', () => {
+			const faker = Genre.fake().aGenre();
+			const categoryId = new CategoryId();
+			const $this = faker.addCategoryId(categoryId);
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_categories_id']).toStrictEqual([categoryId]);
+
+			const otherCategoryId = new CategoryId();
+			faker.addCategoryId(() => otherCategoryId);
+			// @ts-expect-error _categories_id[1] is callable
+			expect(faker['_categories_id'][1]()).toBe(otherCategoryId);
+			expect(faker.categories_id).toStrictEqual([categoryId, otherCategoryId]);
+		});
+
+		test('withCategoriesId', () => {
+			const faker = Genre.fake().aGenre();
+			const categoryId = new CategoryId();
+			const otherCategoryId = new CategoryId();
+			faker.addCategoryId(new CategoryId());
+
+			const $this = faker.withCategoriesId(categoryId, otherCategoryId);
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_categories_id']).toStrictEqual([
+				categoryId,
+				otherCategoryId,
+			]);
+			expect(faker.categories_id).toStrictEqual([categoryId, otherCategoryId]);
+		});
+
+		it('should pass index to categories_id factory', () => {
+			const categoriesId = [new CategoryId(), new CategoryId()];
+			const faker = Genre.fake().aGenre();
+			faker.addCategoryId((index) => categoriesId[index]);
+
+			const genre = faker.build();
+			expect(genre.categories_id.get(categoriesId[0].id)).toBe(categoriesId[0]);
+
+			const fakerMany = Genre.fake().theGenres(2);
+			fakerMany.addCategoryId((index) => categoriesId[index]);
+			const genres = fakerMany.build();
+
+			expect(genres[0].categories_id.get(categoriesId[0].id)).toBe(
+				categoriesId[0],
+			);
+			expect(genres[1].categories_id.get(categoriesId[1].id)).toBe(
+				categoriesId[1],
+			);
+		});
+	});
+
+	describe('is_active prop', () => {
+		const faker = Genre.fake().aGenre();
+
+		it('should be a function', () => {
+			expect(typeof faker['_is_active']).toBe('function');
+		});
+
+		test('activate', () => {
+			const $this = faker.activate();
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_is_active']).toBe(true);
+			expect(faker.is_active).toBe(true);
+		});
+
+		test('deactivate', () => {
+			const $this = faker.deactivate();
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_is_active']).toBe(false);
+			expect(faker.is_active).toBe(false);
+		});
+	});
+
+	describe('created_at prop', () => {
+		const faker = Genre.fake().aGenre();
+
+		it('should throw an error when any `with` method is called', () => {
+			const fakerGenre = Genre.fake().aGenre();
+			expect(() => fakerGenre.created_at).toThrow(
+				new Error("Property created_at not have a factory, use 'with' methods")
+			);
+		});
+
+		it('should be undefined', () => {
+			expect(faker['_created_at']).toBeUndefined();
+		});
+
+		test('withCreatedAt', () => {
+			const date = new Date();
+			const $this = faker.withCreatedAt(date);
+			expect($this).toBeInstanceOf(GenreFakeBuilder);
+			expect(faker['_created_at']).toBe(date);
+
+			faker.withCreatedAt(() => date);
+			// @ts-expect-error _created_at is callable
+			expect(faker['_created_at']()).toBe(date);
+			expect(faker.created_at).toBe(date);
+		});
+
+		it('should pass index to created_at factory', () => {
+			const date = new Date();
+			faker.withCreatedAt((index) => new Date(date.getTime() + index + 2));
+			const genre = faker.build();
+			expect(genre.created_at.getTime()).toBe(date.getTime() + 2);
+
+			const fakerMany = GenreFakeBuilder.theGenres(2);
+			fakerMany.withCreatedAt((index) => new Date(date.getTime() + index + 2));
+			const genres = fakerMany.build();
+
+			expect(genres[0].created_at.getTime()).toBe(date.getTime() + 2);
+			expect(genres[1].created_at.getTime()).toBe(date.getTime() + 3);
+		});
+	});
+
+	it('should create a genre', () => {
+		const faker = GenreFakeBuilder.aGenre();
+		let genre = faker.build();
+
+		expect(genre.gende_id).toBeInstanceOf(GenreId);
+		expect(typeof genre.name === 'string').toBeTruthy();
+		expect(genre.categories_id).toBeInstanceOf(Map);
+		expect(genre.categories_id.size).toBe(1);
+		expect(genre.is_active).toBe(true);
+		expect(genre.created_at).toBeInstanceOf(Date);
+
+		const created_at = new Date();
+		const genre_id = new GenreId();
+		const categoryId = new CategoryId();
+		genre = faker
+			.withGenreId(genre_id)
+			.withName('name test')
+			.withCategoriesId(categoryId)
+			.deactivate()
+			.withCreatedAt(created_at)
+			.build();
+
+		expect(genre.gende_id.id).toBe(genre_id.id);
+		expect(genre.name).toBe('name test');
+		expect(genre.categories_id).toEqual(
+			new Map([[categoryId.id, categoryId]]),
+		);
+		expect(genre.is_active).toBe(false);
+		expect(genre.created_at).toBe(created_at);
+	});
+
+	it('should create many genres', () => {
+		const faker = GenreFakeBuilder.theGenres(2);
+		let genres = faker.build();
+
+		genres.forEach((genre) => {
+			expect(genre.gende_id).toBeInstanceOf(GenreId);
+			expect(typeof genre.name === 'string').toBeTruthy();
+			expect(genre.categories_id).toBeInstanceOf(Map);
+			expect(genre.categories_id.size).toBe(1);
+			expect(genre.is_active).toBe(true);
+			expect(genre.created_at).toBeInstanceOf(Date);
+		});
+
+		const created_at = new Date();
+		const genre_id = new GenreId();
+		const categoryId = new CategoryId();
+		genres = faker
+			.withGenreId(genre_id)
+			.withName('name test')
+			.withCategoriesId(categoryId)
+			.deactivate()
+			.withCreatedAt(created_at)
+			.build();
+
+		genres.forEach((genre) => {
+			expect(genre.gende_id.id).toBe(genre_id.id);
+			expect(genre.name).toBe('name test');
+			expect(genre.categories_id).toEqual(
+				new Map([[categoryId.id, categoryId]]),
+			);
+			expect(genre.is_active).toBe(false);
+			expect(genre.created_at).toBe(created_at);
+		});
+	});
+});
