42c195ab39bd97e20548ea09e85b485c6288846c
Denison Junior <denisonprobable13@gmail.com>
2026-08-17 18:17:01 -0300

    feat: add validation for categories ids existence

---
+++ src/core/category/application/validations/categories_ids_exists_in_storage.validator.ts
@@ -0,0 +1,18 @@
+import {ICategoryRepository} from "@core/category/domain/category.repository";
+import {Category, CategoryId} from "@core/category/domain/category.aggregate";
+import {Either} from "@core/@shared/domain/either";
+import {NotFoundError} from "@core/@shared/domain/errors/not_found.error";
+
+export class CategoriesIdsExistsInStorageValidator {
+	constructor(private categoryRepo: ICategoryRepository) {}
+
+	async validate(categories_id: string[]): Promise<Either<CategoryId[], NotFoundError[]>> {
+		const categoriesId = categories_id.map(id => new CategoryId(id));
+
+		const existsResult = await this.categoryRepo.existsById(categoriesId);
+
+		return existsResult.not_exists.length > 0
+			? Either.fail(existsResult.not_exists.map(c => new NotFoundError(c.id, Category)))
+			: Either.ok(categoriesId);
+	}
+}
