e62017776a190c94b025f03f3c80a2dd6824a6e4
Denison Junior <denisonprobable13@gmail.com>
2026-09-01 10:20:20 -0300

    feat: enhance `AggregateRoot` with domain event management using `eventemitter2`

--- src/core/@shared/domain/aggregate_root.ts
+++ src/core/@shared/domain/aggregate_root.ts
@@ -1,9 +1,19 @@
 import {Entity} from "@core/@shared/domain/entity";
 import {IDomainEvent} from "@core/@shared/domain/events/domain_event.interface";
+import {EventEmitter2} from "eventemitter2";
+
 
 export abstract class AggregateRoot extends Entity {
-	// Vai disparar o evento dentro do próprio agregado
-	applyEvent(event: IDomainEvent) {}
+	events: Set<IDomainEvent> =  new Set();
+	localMediator = new EventEmitter2();
 
-	registerHandler(event: string, handler: (event: IDomainEvent) => void) {}
+	// Vai disparar o evento dentro do próprio agregado
+	applyEvent(event: IDomainEvent) {
+		this.events.add(event);
+		this.localMediator.emit(event.constructor.name, event);
+	}
+
+	registerHandler(event: string, handler: (event: IDomainEvent) => void) {
+		this.localMediator.on(event, handler);
+	}
 }
