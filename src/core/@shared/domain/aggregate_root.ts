a82d36439eb918750c7a241d74313eda879198e3
Denison Junior <denisonprobable13@gmail.com>
2026-09-01 10:08:08 -0300

    feat: introduce base `AggregateRoot` methods for event handling and add `IDomainEvent` interface

--- src/core/@shared/domain/aggregate_root.ts
+++ src/core/@shared/domain/aggregate_root.ts
@@ -1,3 +1,9 @@
 import {Entity} from "@core/@shared/domain/entity";
+import {IDomainEvent} from "@core/@shared/domain/events/domain_event.interface";
 
-export abstract class AggregateRoot extends Entity {}
+export abstract class AggregateRoot extends Entity {
+	// Vai disparar o evento dentro do próprio agregado
+	applyEvent(event: IDomainEvent) {}
+
+	registerHandler(event: string, handler: (event: IDomainEvent) => void) {}
+}
